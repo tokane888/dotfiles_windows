c24483cfc98c491abceae3d655148d1e22e88c9f
tokane888 <tokane888@gmail.com>
2020-08-10 13:51:36 +0900

    各種パッケージをインストールするpowershell script追加

---
+++ install.ps1
@@ -0,0 +1,13 @@
+# kindleはinstall失敗(403 error)したのでブラウザからinstall
+# googlejapaneseinputはダイアログが出るので手動で入れる
+# cinst --yes googlejapanese input
+# TODO: packages.config？使う方法が良いのかもしれないので調査
+cinst --yes autohotkey bind-toolsonly docker-desktop dropbox Everything git googledrive go manictime microsoft-teams microsoft-windows-terminal powershell-core SakuraEditor slack vim virtualbox vlc vscode winmerge wireshark zoom
+
+reg add HKLM\SOFTWARE\Classes\*\shell\sakuraeditor /d "SAKURAで開く(&E)"
+reg add HKLM\SOFTWARE\Classes\*\shell\sakuraeditor\command /t REG_SZ /d "C:\Program Files (x86)\sakura\sakura.exe %1"
+
+Set-Itemproperty -path 'HKCU:\Control Panel\Accessibility\StickyKeys' -Name 'Flags' -value '0'
+
+# git-bashでのgit log参照時などの文字化け対策
+[System.Environment]::SetEnvironmentVariable("LESSCHARSET", "utf-8", "Machine")
