3545080644c7314b3d4fc3a8437bb72bd2e80ecc
tokane888 <tokane888@gmail.com>
2020-08-28 08:23:15 +0900

    picotorrent追加

--- install.ps1
+++ install.ps1
@@ -2,7 +2,7 @@
 # googlejapaneseinputはダイアログが出るので手動で入れる
 # cinst --yes googlejapanese input
 # TODO: packages.config？使う方法が良いのかもしれないので調査
-cinst --yes autohotkey bind-toolsonly docker-desktop dropbox etcher Everything git googledrive go lhaplus manictime microsoft-teams microsoft-windows-terminal powershell-core SakuraEditor slack vim virtualbox vlc vscode winmerge wireshark zoom
+cinst --yes autohotkey bind-toolsonly docker-desktop dropbox etcher Everything git googledrive go lhaplus manictime microsoft-teams microsoft-windows-terminal picotorrent powershell-core SakuraEditor slack vim virtualbox vlc vscode winmerge wireshark zoom
 
 reg add HKLM\SOFTWARE\Classes\*\shell\sakuraeditor /d "SAKURAで開く(&E)"
 reg add HKLM\SOFTWARE\Classes\*\shell\sakuraeditor\command /t REG_SZ /d "C:\Program Files (x86)\sakura\sakura.exe %1"
